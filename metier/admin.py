9caffcd6a7359d9a6f87a308b1cd2fab50787cfa
raymond.warnord <raymond.warnod@eleve.ensai.fr>
2023-09-30 10:03:11 +0200

    fgfgfgllllalalala

--- metier/admin.py
+++ metier/admin.py
@@ -1,4 +1,8 @@
 from metier.prof import Prof
+from metier.eleve import Eleve
+from metier.critere import Critere
+from metier.stage import Stage
+
 
 class Admin(Prof):
     """ Un Admin hérite de la classe Prof 
@@ -8,9 +12,11 @@ class Admin(Prof):
     def __init__(self, critere, email, mdp, code_insee_residence, souhaite_alertes: bool):
         super().__init__(critere, email, mdp, code_insee_residence, souhaite_alertes)
     
-    def supprime_user(self, idUser: str):
-        pass
-    def supprime_critere(self, idCrit: str):
-        pass
-    def supprimer_stage(self):
-        pass
+    def supprime_user(self, unUser: Eleve):
+        unUser.supprimer_compte()
+
+    def supprime_critere(self, unCritere: Critere):
+        unCritere.supprimer_critere()
+
+    def supprimer_stage(self, unStage: Stage):
+        unStage.supprimer_stage()
