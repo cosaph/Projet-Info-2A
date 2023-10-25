359e26ab7cdb52040098006dad2fdc49312972dc
05mery <ouyassin.mariyam@gmail.com>
2023-10-25 20:59:04 +0200

    1er ajout des tests_metier

---
+++ metier/tests_mary/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+from unittest import TestCase, TextTestRunner, TestLoader
+from metier.eleve import Eleve   
+from metier.critere import Critere
+from metier.satge import Stage
---
+++ metier/tests_mary/test_listEnvie.py
@@ -0,0 +1,57 @@
+from unittest import TestCase, TextTestRunner, TestLoader
+from metier.listEnvie import ListEnvie
+from metier.stage import Stage 
+
+class TestListEnvie(TestCase):
+
+    #Tester la méthode __str__()
+    def test___str__(self):
+        # GIVEN
+        stage = Stage()  # Initialisez comme nécessaire
+        liste = ListEnvie([stage])
+
+        # WHEN
+        resultat = liste.__str__()
+
+        # THEN
+        self.assertEqual(resultat, stage.__str__() + "\n")
+
+        # Test avec une liste vide
+        liste_vide = ListEnvie()
+        
+        # WHEN
+        resultat_vide = liste_vide.__str__()
+
+        # THEN
+        self.assertEqual(resultat_vide, "Pas de stages dans la liste d'envie")
+
+    #Tester la méthode ajouter_stage()
+    def test_ajouter_stage(self):
+        # GIVEN
+        liste = ListEnvie()
+        stage = Stage()  # Initialisez comme nécessaire
+
+        # WHEN
+        liste.ajouter_stage(stage)
+
+        # THEN
+        self.assertIn(stage, liste.listStage)
+
+    #Tester la méthode supprimer_stage()
+    def test_supprimer_stage(self):
+        # GIVEN
+        stage = Stage()  # Initialisez comme nécessaire
+        liste = ListEnvie([stage])
+
+        # WHEN
+        liste.supprimer_stage(stage)
+
+        # THEN
+        self.assertNotIn(stage, liste.listStage)
+
+
+if __name__ == "__main__":
+    # Run the tests
+    result = TextTestRunner(verbosity=2).run(
+        TestLoader().loadTestsFromTestCase(TestListEnvie)
+    )
