35aacbf0502e773edce426674313c5d8db0f4d9b
05mery <ouyassin.mariyam@gmail.com>
2023-10-26 16:41:40 +0200

    knb

--- test/test_business_object/test_eleve/testChargerUser.py
+++ test/test_business_object/test_eleve/testChargerUser.py
@@ -2,60 +2,39 @@ import sys
 sys.path.insert(0,"D:/Cours/S1/Projet informatique 2A/Code ProjetInfo/Projet-Info-2A")
 import metier.eleve
 
-
 from unittest import TestCase, TextTestRunner, TestLoader
+from unittest.mock import patch
 from metier.eleve import Eleve
 
 class TestEleve(TestCase):
     
-    # Tester la méthode charger_user()
-    def test_charger_user(self):
+    @patch('metier.eleve.UserDao.charger_user')
+    @patch('metier.eleve.AssoCritUserDao.exist_email')
+    @patch('metier.eleve.AssoStageUserDao.exist_email')
+    def test_charger_user(self, mock_exist_email_stage, mock_exist_email_crit, mock_charger_user):
         # GIVEN
         email = 'test@example.com'
         mdp = '123456'
+        mock_charger_user.return_value = {
+            'email': email,
+            'mdp': mdp,
+            'code_insee_residence': None,
+            'souhaite_alertes': False,
+            'profil': 'Eleve'
+        }
+        mock_exist_email_crit.return_value = False
+        mock_exist_email_stage.return_value = False
         
         # WHEN
         eleve = Eleve.charger_user(email, mdp)
         
         # THEN
-        #self.assertIsInstance(eleve, Eleve)  
-        self.assertEqual(eleve.email, email)  
+        self.assertIsInstance(eleve, Eleve)
+        self.assertEqual(eleve.email, email)
+
 
 if __name__ == "__main__":
     # Run the tests
     result = TextTestRunner().run(
         TestLoader().loadTestsFromTestCase(TestEleve)
     )
-
-
-"""  
-
-
-
-
-
-
-    from unittest.mock import patch
-    #Tester la méthode rechercher_stage()
-    def test_rechercher_stage(self):
-        eleve = Eleve()  # Initialisez comme nécessaire
-        critere = Critere()  # Initialisez comme nécessaire
-
-        # Mock du retour de la méthode `recherche_stage` de la classe `Critere`
-        mock_return_value = [Stage(), Stage()]  # Assumez que Stage est une classe que vous avez définie
-        with patch.object(Critere, 'recherche_stage', return_value=mock_return_value):
-            # Assurez-vous que l'élève a des critères
-            eleve.ajouter_critereAuser(critere)  
-
-            # Test de la méthode
-            result = eleve.rechercher_stage(critere)
-
-        # Vérifiez que la méthode retourne une liste de stages
-        self.assertIsInstance(result, list)
-        self.assertEqual(len(result), len(mock_return_value))
- """
-""" if __name__ == "__main__":
-    result = TextTestRunner(verbosity=2).run(
-        TestLoader().loadTestsFromTestCase(TestEleve)
-    )
- """
