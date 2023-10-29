1c989535432245e8f61e97c6a152ab0633bcc7b2
COTTET Coralie <id2263@DOMENSAI.ECOLE>
2023-10-29 11:52:41 +0100

    est

--- metier/critere.py
+++ metier/critere.py
@@ -42,32 +42,28 @@ class Critere:
         
     def recherche_stage(critere, localisation, rayon):
 
-
-        params = {'q': critere, 'l': localisation, 'stage': critere, 'location': localisation, 'radius': rayon}
+        params = {'q': critere, 'l': localisation,'stage': critere, 'location': localisation,'radius':rayon}
         url = requests.Request('GET', 'https://www.stage.fr/jobs/', params=params).prepare().url
-        """ url = requests.Request('GET', 'https://emploi.lefigaro.fr/', params=params).prepare().url """
 
         response = requests.get(url)
 
         soup = BeautifulSoup(response.text, 'html.parser')
         div_elements = soup.find_all('div', class_='media-heading listing-item__title')
-        span_elements = soup.find_all('span', class_='listing-item__info--item listing-item__info--item-location')
+        span_elements = soup.find_all('span', class_ = 'listing-item__info--item listing-item__info--item-location')
 
         tableau = []
+
+
         for i in range(len(div_elements)):
             link_elements = div_elements[i].find_all('a')
-            # rrrrrrrrrrrrrr
             location_element = span_elements[i].text
+
             for link_element in link_elements:
-                tableau.append(
-                    Stage(
-                        url_stage=link_element['href'],
-                        titre=link_element.text,
-                        specialite= critere,
-                        ville=location_element
-                        )
-                            )
+                title = link_element.text
+                url = link_element['href']
+                tableau.append([title, url, location_element])
         print(tableau)
+
     
     def existe(self):
         return CritereDAO().exist_id(self)
